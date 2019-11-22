ab5a8087a614592c1591baab8a39b5b30f3232e0
Urchmaney <kingsobino@gmail.com>
2019-11-22 23:24:47 +0000

    Resolve stickler error

--- src/components/buttonPanel.js
+++ src/components/buttonPanel.js
@@ -1,42 +1,34 @@
 import React from 'react';
 import Button from './button';
 
+const renderButton = name => (<Button name={name} />);
+
 export default class ButtonPanel extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.renderButton = this.renderButton;
-  }
-
-  renderButton(name) {
-    return (<Button name={name} />);
-  }
-
-  render(){
+  render() {
     return (
       <div>
         <div>
-          { this.renderButton('%') }
-          { this.renderButton('+/-') }
-          { this.renderButton('AC') }
-          { this.renderButton('/') }
+          { renderButton('%') }
+          { renderButton('+/-') }
+          { renderButton('AC') }
+          { renderButton('/') }
         </div>
         <div>
-          { this.renderButton('7') }
-          { this.renderButton('8') }
-          { this.renderButton('9') }
-          { this.renderButton('X') }
+          { renderButton('7') }
+          { renderButton('8') }
+          { renderButton('9') }
+          { renderButton('X') }
         </div>
         <div>
-          { this.renderButton('4') }
-          { this.renderButton('5') }
-          { this.renderButton('6') }
-          { this.renderButton('-') }
+          { renderButton('4') }
+          { renderButton('5') }
+          { renderButton('6') }
+          { renderButton('-') }
         </div>
         <div>
-          { this.renderButton('0') }
-          { this.renderButton('.') }
-          { this.renderButton('=') }
+          { renderButton('0') }
+          { renderButton('.') }
+          { renderButton('=') }
         </div>
       </div>
     );
